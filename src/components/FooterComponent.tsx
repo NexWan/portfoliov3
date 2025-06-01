f93017bf3d94d3a63789799b0f05616730517d01
NexWan <lcontrerasmartinez235@gmail.com>
2025-06-01 02:42:54 -0600

    Fixed an small issue with the Z index of the footer and added the links to my socials

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,14 +1,13 @@
 import { MailOutlined, TwitterOutlined, GithubOutlined, DiscordOutlined, LinkedinOutlined } from "@ant-design/icons";
  
  function FooterComponent() {
-    return <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 flex justify-around items-center">
+    return <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 flex justify-around items-center z-10">
       <nav className="*:2xl:text-lg">
         <h6 className="footer-title 2xl:text-2xl">Contact</h6>
-        <a className="link link-hover"><MailOutlined /> <span>Email</span></a>
-        <a className="link link-hover"><TwitterOutlined /> Twitter</a>
-        <a className="link link-hover"><GithubOutlined /> GitHub</a>
-        <a className="link link-hover"><DiscordOutlined /> Discord</a>
-        <a className="link link-hover"><LinkedinOutlined /> LinkedIn</a>
+        <a className="link link-hover" href="mailto:lcontrerasmartinez235@gmail.com" target="_blank"><MailOutlined /> <span>Email</span></a>
+        <a className="link link-hover" href="https://x.com/OddysDaWan" target="_blank"><TwitterOutlined /> Twitter</a>
+        <a className="link link-hover" href="https://github.com/NexWan" target="_blank" ><GithubOutlined /> GitHub</a>
+        <a className="link link-hover" href="https://www.linkedin.com/in/leonardo-contreras-martinez-a30843229/"><LinkedinOutlined /> LinkedIn</a>
       </nav>
       <div className="divider divider-horizontal"></div>
       <nav className="*:2xl:text-lg">
