cd685cad5d6eee937cba670266d151d793b914fc
NexWan <lcontrerasmartinez235@gmail.com>
2025-05-31 20:40:46 -0600

    Vercel configuration for SPAs and removed the analytics since it didn't work

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -3,12 +3,10 @@ import { MouseTrail } from "@stichiboi/react-elegant-mouse-trail";
 import Navbar from "./components/Navbar";
 import { useThemeStore } from "./stores/themeStore";
 import FooterComponent from "./components/FooterComponent";
-import { Analytics } from "@vercel/analytics/next"
 function AppLayout() {
   const theme = useThemeStore((state) => state.theme);
   return (
     <>
-    <Analytics />
     <MouseTrail strokeColor={theme === "valentine" ? "#22223b" : "#f0f0f0"} />
     <div className="flex flex-col h-screen w-screen overflow-auto">
       <header className="flex 2xl:max-h-1/3 items-start justify-start bg-neutral text-neutral-content sticky top-0 z-50">
